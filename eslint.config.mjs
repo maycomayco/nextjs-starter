2b403ff9cb61307a154a2e7702b50861dc191b98
Mayco Barale <maycobarale@gmail.com>
2025-01-21 11:11:52 -0300

    Initial commit with starter config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // ...compat.extends("next/core-web-vitals", "next/typescript","prettier"),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    rules: {
+      'prefer-arrow-callback': 'error',
+      'no-console': 'warn',
+      'prefer-template': 'error',
+      semi: 'error',
+      quotes: ['error', 'double'],
+    },
+  }),
+  eslintPluginPrettierRecommended,
 ];
 
 export default eslintConfig;
